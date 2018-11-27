1d5bbb69b0e743ef9ff19a80a4c9b13118a074f8
Josh Mischung <josh.mischung@gmail.com>
2018-11-27 11:25:17 -0800

    ec2_airflow_scp.sh: correct transferred file

--- ec2_airflow_scp.sh
+++ ec2_airflow_scp.sh
@@ -4,4 +4,4 @@
 # scp Airflow install and setup script to ec2 instance
 #
 EC2_HOSTNAME=$(cat ./.ec2_hostname)
-scp -r ~/.ssh/ -r ~/Documents/Agile_Data_Code_2/ec2_airflow_scp.sh ubuntu@$EC2_HOSTNAME:/home/ubuntu/
+scp -i ~/.ssh/agile_data_science.pem -r ~/Documents/Agile_Data_Code_2/aws/airflow_install.sh ubuntu@$EC2_HOSTNAME:/home/ubuntu/
